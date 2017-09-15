24982806f089ef368f1547ab3de4833a8a7b73e8
Jonathan Mines <minesja@gmail.com>
2017-09-15 22:05:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 
-/* describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logShout('hello')
-
-    expect(spy).toHaveBeenCalledWith('HELLO')
-
-    console.log.restore()
-  })
-}) */
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+  }
+  else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma."){
+    return "I love you, too."
+  }
+}
