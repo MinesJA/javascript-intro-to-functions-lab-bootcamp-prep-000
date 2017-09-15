5372e34d1a177e9c5a9bdc6a4c5ca1004eaa8e9f
Jonathan Mines <minesja@gmail.com>
2017-09-15 21:51:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+  console.log(string.toLowerCase);
+}
+
+/* describe('logShout(string)', function() {
+  it('calls console.log() its one argument in all caps', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
+  })
+}) */
